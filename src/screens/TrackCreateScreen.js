1012ab23a6567eef4dfc49f0ea86721e91b295fe
Tomas Kutnius <tomas.kutniauskis@gmail.com>
2025-03-03 15:46:30 +0100

    Add LocationContext and integrate location tracking in Map and TrackCreateScreen

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,5 +1,5 @@
 import '../_mockLocation';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import {
@@ -8,9 +8,11 @@ import {
   Accuracy,
 } from 'expo-location';
 import Map from '../components/Map';
+import { Context as LocationContext } from '../context/LocationContext';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
   const [err, setErr] = useState(null);
 
   const startWatching = async () => {
@@ -26,7 +28,7 @@ const TrackCreateScreen = () => {
           distanceInterval: 10,
         },
         (location) => {
-          console.log(location);
+          addLocation(location);
         }
       );
     } catch (error) {
