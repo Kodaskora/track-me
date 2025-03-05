b0209797495d2eb902cfeb451445e876a58ce80f
Tomas Kutnius <tomas.kutniauskis@gmail.com>
2025-03-05 13:31:31 +0100

    Add TrackContext and integrate track saving functionality in TrackForm

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
-  baseURL: 'https://689f-158-248-26-98.ngrok-free.app',
+const instance = axios.create({
+  baseURL: 'https://fc75-158-248-26-98.ngrok-free.app',
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import { Context as LocationContext } from '../context/LocationContext';
 import Spacer from './Spacer';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 const TrackForm = () => {
   const {
@@ -10,6 +11,7 @@ const TrackForm = () => {
     stopRecording,
     changeName,
   } = useContext(LocationContext);
+  const [saveTrack] = useSaveTrack();
 
   return (
     <>
@@ -23,7 +25,7 @@ const TrackForm = () => {
       </Spacer>
       <Spacer>
         {!recording && locations.length ? (
-          <Button title='Save Recording' />
+          <Button title='Save Recording' onPress={saveTrack} />
         ) : null}
       </Spacer>
     </>
